0a3714e1a008e7eca36e7276494f374da3c7d5ce
arheard <arheard0@gmail.com>
2019-01-21 21:17:14 -0500

    Adds protocol message handlers

--- multilogue.go
+++ multilogue.go
@@ -1,5 +1,7 @@
 package main
 
+import inet "github.com/libp2p/go-libp2p-net"
+
 // pattern: /protocol-name/request-or-response-message/version
 const clientSendMessage = "/multilogue/clientsendmessage/0.0.1"
 const clientTransmissionStart = "/multilogue/clienttransmissionstart/0.0.1"
@@ -19,7 +21,29 @@ func NewMultilogueProtocol(node *Node) *MultilogueProtocol {
 	p := &MultilogueProtocol{
 		node: node}
 
-	//node.SetStreamHandler(gravitationRequest, p.onGravitationRequest)
-	//node.SetStreamHandler(gravitationResponse, p.onGravitationResponse)
+	node.SetStreamHandler(clientSendMessage, p.onClientSendMessage)
+	node.SetStreamHandler(clientTransmissionStart, p.onClientTransmissionStart)
+	node.SetStreamHandler(clientTransmissionEnd, p.onClientTransmissionEnd)
+	node.SetStreamHandler(hostAcceptTransmission, p.onHostAcceptTransmission)
+	node.SetStreamHandler(hostDenyTransmission, p.onHostDenyTransmission)
+	node.SetStreamHandler(hostBroadcastMessage, p.onHostBroadcastMessage)
 	return p
+}
+
+func (p *MultilogueProtocol) onClientSendMessage(s inet.Stream) {
+}
+
+func (p *MultilogueProtocol) onClientTransmissionStart(s inet.Stream) {
+}
+
+func (p *MultilogueProtocol) onClientTransmissionEnd(s inet.Stream) {
+}
+
+func (p *MultilogueProtocol) onHostAcceptTransmission(s inet.Stream) {
+}
+
+func (p *MultilogueProtocol) onHostDenyTransmission(s inet.Stream) {
+}
+
+func (p *MultilogueProtocol) onHostBroadcastMessage(s inet.Stream) {
 }
