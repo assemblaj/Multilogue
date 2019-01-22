1552666e7062ef46f6e6e50a5b6d2cfc67233269
arheard <arheard0@gmail.com>
2019-01-21 20:05:47 -0500

    Adds multilogue and main files
    Multilogue file contains protocol message definitions

---
+++ multilogue.go
@@ -0,0 +1,9 @@
+package main
+
+// pattern: /protocol-name/request-or-response-message/version
+const clientSendMessage = "/multilogue/clientsendmessage/0.0.1"
+const clientTransmissionStart = "/multilogue/clienttransmissionstart/0.0.1"
+const clientTransmissionEnd = "/multilogue/clienttransmissionend/0.0.1"
+const hostAcceptTransmission = "/multilogue/hostaccepttransmission/0.0.1"
+const hostDenyTransmission = "/multilogue/hostdenytransmission/0.0.1"
+const hostBroadcastMessage = "/multilogue/hostbroadcastmessage/0.0.1"
