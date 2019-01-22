07e9bd504acbb3d9ced4f08bfdcfcdfba473b05c
arheard <arheard0@gmail.com>
2019-01-21 21:05:37 -0500

    Adds Node.go file

--- multilogue.go
+++ multilogue.go
@@ -7,3 +7,19 @@ const clientTransmissionEnd = "/multilogue/clienttransmissionend/0.0.1"
 const hostAcceptTransmission = "/multilogue/hostaccepttransmission/0.0.1"
 const hostDenyTransmission = "/multilogue/hostdenytransmission/0.0.1"
 const hostBroadcastMessage = "/multilogue/hostbroadcastmessage/0.0.1"
+
+// MultilogueProtocol type
+type MultilogueProtocol struct {
+	node *Node // local host
+}
+
+// NewMultilogueProtocol Create instance of protocol
+// Might want a data object
+func NewMultilogueProtocol(node *Node) *MultilogueProtocol {
+	p := &MultilogueProtocol{
+		node: node}
+
+	//node.SetStreamHandler(gravitationRequest, p.onGravitationRequest)
+	//node.SetStreamHandler(gravitationResponse, p.onGravitationResponse)
+	return p
+}
